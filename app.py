767e0f3aca706da99ab8e444d0143cc35fa68428
Sion Kim <skim14@academic.rrc.ca>
2024-07-23 19:25:34 -0500

    Update app.py with demo

--- app.py
+++ app.py
@@ -54,5 +54,7 @@ def download_file(filename):
 # Start Flask app in debug mode
 # if __name__ == '__main__':
 #     app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
